d269f724687029a4092b493b78739ba20d19d67e
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 10:44:02 +0000

    Fri Jul 22 10:44:02 UTC 2022

--- lib/solutions/SUM/test_sum.py
+++ lib/solutions/SUM/test_sum.py
@@ -1,12 +1,13 @@
-import pytest
-from calculations import sum
+# import pytest
+# from calculations import sum
 
 
-def test_sum_works():
-    assert sum(1, 3) == 4, "sum calculation failed"
+# def test_sum_works():
+#     assert sum(1, 3) == 4, "sum calculation failed"
 
 
-def test_sum_arg_out_of_bounds():
-    with pytest.raises(ValueError) as exc_info:
-        sum(-1, 3)
-    assert "Argument out of bounds" in str(exc_info.value)
+# def test_sum_arg_out_of_bounds():
+#     with pytest.raises(ValueError) as exc_info:
+#         sum(-1, 3)
+#     assert "Argument out of bounds" in str(exc_info.value)
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,16 @@
+import pytest
 from solutions.SUM import sum_solution
 
 
-class TestSum():
+class TestSum:
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_works(self):
+        assert sum(1, 3) == 4, "sum calculation failed"
+
+    def test_sum_arg_out_of_bounds(self):
+        with pytest.raises(ValueError) as exc_info:
+            sum(-1, 3)
+        assert "Argument out of bounds" in str(exc_info.value)
+
