418f1d881c54644cfc37dd46861284262544790b
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 12:02:28 +0000

    Fri Jul 22 12:02:28 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -9,5 +9,7 @@ class TestCheckout:
         assert checkout_solution.checkout("AAA") == 130
         assert checkout_solution.checkout("AF") == 49
         assert checkout_solution.checkout("AAAAAAF") == 259
+        assert checkout_solution.checkout("BABACAGA") == 45 + 130 + 20 - 1 + 50
+
 
 
