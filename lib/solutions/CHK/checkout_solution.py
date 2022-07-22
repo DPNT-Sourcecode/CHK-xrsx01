8a9a3241520a36b45783d980e8d0a954000aaee8
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 11:11:59 +0000

    Fri Jul 22 11:11:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,27 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    items = {
+        "A": {
+            "price": 50,
+            "deal": {
+                "amount": 3,
+                "saved": 20,
+            },
+        },
+        "B": {
+            "price": 30,
+            "deal": {
+                "amount": 2,
+                "saved": 15,
+            },
+        },
+        "C": {
+            "price": 20,
+        },
+        "D": {
+            "price": 15,
+        },
+    }
+    args = skus.split("")
+
