9d6516253fda2161bdf69f2f38f75c645066e9f6
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 11:00:48 +0000

    Fri Jul 22 11:00:48 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,4 +1,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
-    return f"Hello, {friend_name}"
+    # return f"Hello, {friend_name}"
+    return "Hello, World!"
+
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -4,4 +4,6 @@ from solutions.HLO import hello_solution
 
 class TestHello:
     def test_hello(self):
-        assert hello_solution.hello("iwoca") == "Hello, iwoca"
+        # assert hello_solution.hello("iwoca") == "Hello, iwoca"
+        assert hello_solution.hello("iwoca") == "Hello, World!"
+
