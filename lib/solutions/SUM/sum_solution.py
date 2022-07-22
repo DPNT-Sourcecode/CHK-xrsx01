595fc1383687c467d4d4d2c11d77d149f4dbdadf
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 10:41:02 +0000

    Fri Jul 22 10:41:02 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     raise NotImplementedError()
+
+
+def sum(int_1: int, int_2: int) -> int:
+    if (int_1 or int_2) > 100:
+        raise ValueError("Argument out of bounds")
+    elif (int_1 or int_2) < 0:
+        raise ValueError("Argument out of bounds")
+    # redfining builtin method
+    return int_1 + int_2
+
