2a61e8bf1e6e7bfe3ac1fce5d1b46bac7dbe1d41
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 11:50:59 +0000

    Fri Jul 22 11:50:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ def checkout(skus):
                 "amount": 3,
                 "saved": 20,
             },
+            "count": 0,
         },
         "B": {
             "price": 30,
@@ -15,13 +16,10 @@ def checkout(skus):
                 "amount": 2,
                 "saved": 15,
             },
+            "count": 0,
         },
-        "C": {
-            "price": 20,
-        },
-        "D": {
-            "price": 15,
-        },
+        "C": {"price": 20, "count": 0},
+        "D": {"price": 15, "count": 0},
     }
     output = 0
     for item in skus:
@@ -30,13 +28,13 @@ def checkout(skus):
             output -= 1
         else:
             output += items[item]["price"]
-            items[item]["count"] = (
-                1 if not "count" in items[item] else items[item]["count"] + 1
-            )
+            items[item]["count"] += 1
         if (
             "deal" in items[item]
             and items[item]["count"] == items[item]["deal"]["amount"]
         ):
+            items[item]["count"] = 0
             output -= items[item]["deal"]["saved"]
 
     return output
+
