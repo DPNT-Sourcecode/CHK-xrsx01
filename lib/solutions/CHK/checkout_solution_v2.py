f0a9fb0344d77667150c6ef0c6fc27f613dd3e3b
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 12:15:49 +0000

    Fri Jul 22 12:15:49 UTC 2022

---
+++ lib/solutions/CHK/checkout_solution_v2.py
@@ -0,0 +1,47 @@
+# noinspection PyUnusedLocal
+# skus = unicode string
+def checkout(skus):
+    items = {
+        "A": {
+            "price": 50,
+            "deal": {
+                "amount": 3,
+                "saved": 20,
+            },
+            "count": 0,
+        },
+        "B": {
+            "price": 30,
+            "deal": {
+                "amount": 2,
+                "saved": 15,
+            },
+            "count": 0,
+        },
+        "E": {
+            "price": 40,
+            "deal": {
+                "amount": 2,
+                "saved": 40,
+            },
+            "count": 0,
+        },
+        "C": {"price": 20, "count": 0},
+        "D": {"price": 15, "count": 0},
+    }
+    output = 0
+    for item in skus:
+        if item not in items or item.islower():
+            print("illegal item found")
+            return -1
+        else:
+            output += items[item]["price"]
+            items[item]["count"] += 1
+            if (
+                "deal" in items[item]
+                and items[item]["count"] == items[item]["deal"]["amount"]
+            ):
+                items[item]["count"] = 0
+                output -= items[item]["deal"]["saved"]
+
+    return output
