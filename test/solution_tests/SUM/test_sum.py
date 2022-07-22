b52e69d4f903ee6cd1a5d264db31e08c125c8f37
sbx_user1051 <sbx_user1051@169.254.11.97>
2022-07-22 10:47:02 +0000

    Fri Jul 22 10:47:02 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,10 +7,11 @@ class TestSum:
         assert sum_solution.compute(1, 2) == 3
 
     def test_sum_works(self):
-        assert sum(1, 3) == 4, "sum calculation failed"
+        assert sum_solution.compute(1, 3) == 4, "sum calculation failed"
 
     def test_sum_arg_out_of_bounds(self):
         with pytest.raises(ValueError) as exc_info:
-            sum(-1, 3)
+            sum_solution.compute(-1, 3)
         assert "Argument out of bounds" in str(exc_info.value)
+
 
